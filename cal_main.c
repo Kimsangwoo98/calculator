b03c0a4b4d7480e27a533da4fd1548a7aad3c74b
Kimsangwoo98 <118726819+Kimsangwoo98@users.noreply.github.com>
2022-11-21 18:57:01 +0900

    Update cal_main.c

--- cal_main.c
+++ cal_main.c
@@ -6,13 +6,17 @@ int main(int argc, char *argv[])
 {
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
+    int c = atoi(argv[3]);
+    int d = atoi(argv[4]);
 
     printf("<===============>\n");
     printf("<===============>\n");
     printf("<===============>\n");
     printf("<===============>\n");
     printf("%d + %d => %d\n", a, b, add(a, b));
+    printf("%d + %d => %d\n", c, d, add(c, d));
     printf("%d - %d => %d\n", a, b, sub(a, b));
+    printf("%d - %d => %d\n", c, d, sub(c, d));
     printf("%d * %d => %d\n", a, b, mul(a, b));
     printf("%d / %d => %d\n", a, b, dur(a, b));
     printf("===============\n");
